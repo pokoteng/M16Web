f3a9879bb73c5cc05ce1c82e22a9f30b537b6397
three3year <three3year@yahoo.com.tw>
2019-11-19 00:02:15 +0800

    web map 新增

--- player/src/App.ts
+++ player/src/App.ts
@@ -1,4 +1,9 @@
 import "./index.html";
 import "./game.html";
+import "./room.html";
+import "./counting.html";
+import "./training.html";
+import "./defence.html";
+import "./tools.html";
 import Mahjong from "./M16";
 window.onload = Mahjong;
